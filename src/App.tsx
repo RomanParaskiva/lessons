5f755312dfd8ece471da4ed7f0cbf8a8ffc6d804
RomanParaskiva <parasta2018@gmail.com>
2022-07-14 11:21:46 +0300

    added SB,  chromatik

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Page } from './components/Page'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Page />
     </div>
   )
 }
